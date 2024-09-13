2d749b8a862cfe99288149b36ed5bb0c95525732
Janelle <2264502@cegeplimoilou.ca>
2024-09-13 16:02:37 -0400

    Configuration du projet #1
    Retirer le code inital de Vite + React
    Supprimer le CSS existant
    Importer Bootstrap dans le projet
    Installer NPM

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootswatch/dist/minty/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
