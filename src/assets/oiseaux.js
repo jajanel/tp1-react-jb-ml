3504f4dca106b38debf5cba1b098798421e8d7cc
JanelleB2 <2264502@cegeplimoilou.ca>
2024-09-15 14:33:42 -0400

    Changement JSON à array JS #11

---
+++ src/assets/oiseaux.js
@@ -0,0 +1,182 @@
+export const dataOiseau = [
+  {
+    idOiseau: 1,
+    categorie: "poule",
+    race: "chantecler",
+    prix: 50,
+    origine: "amerique",
+    datePublication: "2024-12-12",
+    srcImage: "src/assets/images/poules/chantecler.jpg"
+  },
+  {
+    idOiseau: 2,
+    categorie: "poule",
+    race: "ayam cemani",
+    prix: 95,
+    origine: "asie",
+    datePublication: "2024-12-02",
+    srcImage: "src/assets/images/poules/ayam.jpg"
+  },
+  {
+    idOiseau: 3,
+    categorie: "poule",
+    race: "silkie",
+    prix: 32,
+    origine: "asie",
+    datePublication: "2024-12-03",
+    srcImage: "src/assets/images/poules/silkie.jpg"
+  },
+  {
+    idOiseau: 4,
+    categorie: "poule",
+    race: "faverolle",
+    prix: 56,
+    origine: "europe",
+    datePublication: "2024-12-01",
+    srcImage: "src/assets/images/poules/faverolle.jpg"
+  },
+  {
+    idOiseau: 5,
+    categorie: "poule",
+    race: "brahma",
+    prix: 40,
+    origine: "amerique",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/poules/brahma.jpg"
+  },
+  {
+    idOiseau: 6,
+    categorie: "poule",
+    race: "rousse",
+    prix: 12,
+    origine: "amerique",
+    datePublication: "2024-12-08",
+    srcImage: "src/assets/images/poules/rousse.jpg"
+  },
+  {
+    idOiseau: 7,
+    categorie: "poule",
+    race: "frizzle",
+    prix: 35,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/poules/frizzle.jpg"
+  },
+  {
+    idOiseau: 8,
+    categorie: "oie",
+    race: "sebastopol",
+    prix: 80,
+    origine: "europe",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/oies/sebastopol.jpg"
+  },
+  {
+    idOiseau: 9,
+    categorie: "oie",
+    race: "bernache",
+    prix: 25,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/oies/bernache.jpg"
+  },
+  {
+    idOiseau: 10,
+    categorie: "oie",
+    race: "blanche",
+    prix: 12,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/oies/blanche.jpg"
+  },
+  {
+    idOiseau: 11,
+    categorie: "oie",
+    race: "cendre",
+    prix: 20,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/oies/cendre.jpg"
+  },
+  {
+    idOiseau: 12,
+    categorie: "oie",
+    race: "chine",
+    prix: 56,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/oies/chine.jpg"
+  },
+  {
+    idOiseau: 13,
+    categorie: "dinde",
+    race: "narrangansett",
+    prix: 90,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/dindes/narragansett.jpg"
+  },
+  {
+    idOiseau: 14,
+    categorie: "dinde",
+    race: "ocellated",
+    prix: 200,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/dindes/ocellated.jpg"
+  },
+  {
+    idOiseau: 15,
+    categorie: "dinde",
+    race: "sauvage",
+    prix: 1,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/dindes/sauvage.jpg"
+  },
+  {
+    idOiseau: 16,
+    categorie: "canard",
+    race: "bain",
+    prix: 1,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/canards/bain.jpg"
+  },
+  {
+    idOiseau: 17,
+    categorie: "canard",
+    race: "branchu",
+    prix: 60,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/canards/branchu.jpg"
+  },
+  {
+    idOiseau: 18,
+    categorie: "canard",
+    race: "colvert",
+    prix: 10,
+    origine: "amerique",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/canards/colvert.jpg"
+  },
+  {
+    idOiseau: 19,
+    categorie: "canard",
+    race: "pekin",
+    prix: 20,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/canards/pekin.jpg"
+  },
+  {
+    idOiseau: 20,
+    categorie: "canard",
+    race: "runner",
+    prix: 68,
+    origine: "asie",
+    datePublication: "2024-12-11",
+    srcImage: "src/assets/images/canards/runner.jpg"
+  }
+];
