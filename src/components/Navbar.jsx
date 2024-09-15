c022a166cface134cec863d5f07ba6b3c639c403
JanelleB2 <2264502@cegeplimoilou.ca>
2024-09-15 14:34:19 -0400

    Ajout handler de choix de catégorie #2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-export default function Navbar() {
+export default function Navbar({ surChangementCategorie }) {
+
+    const handleChoixCategorie = (categorieOiseau) => {
+        surChangementCategorie(categorieOiseau);
+    };
 
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" onClick="">Critique Oiso</a>
+                    <a className="navbar-brand" onClick={() => handleChoixCategorie("tous")}>Critique Oiso</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -13,16 +17,19 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarScroll">
                         <ul className="navbar-nav me-auto my-2 my-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" onClick="">Poules</a>
+                                <a className="nav-link" onClick={() => handleChoixCategorie("tous")}>Tous</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick="">Canards</a>
+                                <a className="nav-link" onClick={() => handleChoixCategorie("poule")}>Poules</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick="">Oies</a>
+                                <a className="nav-link" onClick={() => handleChoixCategorie("canard")}>Canards</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick="">Dindes</a>
+                                <a className="nav-link" onClick={() => handleChoixCategorie("oie")}>Oies</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" onClick={() => handleChoixCategorie("dinde")}>Dindes</a>
                             </li>
                         </ul>
                         <div className="dropdown">
@@ -31,21 +38,19 @@ export default function Navbar() {
                                 Statistiques
                             </button>
                             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                                <li><a className="dropdown-item" onClick="">Poules</a></li>
+                                <li><a className="dropdown-item" onClick={() => handleChoixCategorie('poule')}>Poules</a></li>
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
-                                <li><a className="dropdown-item" onClick="">Canards</a></li>
+                                <li><a className="dropdown-item" onClick={() => handleChoixCategorie('canard')}>Canards</a></li>
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
-                                <li><a className="dropdown-item" onClick="">Oies</a></li>
+                                <li><a className="dropdown-item" onClick={() => handleChoixCategorie('oie')}>Oies</a></li>
                                 <li>
                                     <hr className="dropdown-divider"/>
                                 </li>
-                                <li><a className="dropdown-item" onClick="">Dindes</a></li>
-
-
+                                <li><a className="dropdown-item" onClick={() => handleChoixCategorie('dinde')}>Dindes</a></li>
                             </ul>
                         </div>
                     </div>
