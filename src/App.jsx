0cc11a836c0c8614c52b9f0d52ac41f286143649
JanelleB2 <2264502@cegeplimoilou.ca>
2024-09-15 14:36:08 -0400

    Affichage des cartes produits et handler catégorie #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
+import CarteProduit from "./components/CarteProduit.jsx";
+import {dataOiseau} from "./assets/oiseaux.js";
+import { useState } from 'react';
 
 function App() {
+    const [categorieSelectionne, setCategorieSelectionne] = useState('all');
 
-  return (
-    <>
-      <Navbar/>
-    </>
-  )
+    const handleChangementCategorie = (categorieOiseau) => {
+        setCategorieSelectionne(categorieOiseau);
+    };
+
+    let oiseauxFiltre;
+    if (categorieSelectionne === "tous") {
+        oiseauxFiltre = dataOiseau;
+    } else {
+        oiseauxFiltre = dataOiseau.filter(oiseau => oiseau.categorie === categorieSelectionne);
+    }
+
+
+    return (
+        <>
+            <Navbar surChangementCategorie={handleChangementCategorie} />
+            <div className="container-fluid">
+                <div className="row">
+                    {
+                        oiseauxFiltre.map((oiseau) => {
+                            return (
+                                <div className="col-xl-4 col-xxl-3 col-md-12 col-lg-6 align-content-center" key={oiseau.idOiseau}>
+                                    <CarteProduit
+                                        categorie={oiseau.categorie}
+                                        race={oiseau.race}
+                                        origine={oiseau.origine}
+                                        prix={oiseau.prix}
+                                        srcImage={oiseau.srcImage}
+                                        datePublication={oiseau.datePublication}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App
+export default App;
