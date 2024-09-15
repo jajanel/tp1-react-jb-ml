53e0363f6b400b67e5878cc73f386674e6b0bd8e
JanelleB2 <2264502@cegeplimoilou.ca>
2024-09-15 12:28:12 -0400

    Changement du thème Bootswatch #1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootswatch/dist/minty/bootstrap.min.css'
+import 'bootswatch/dist/lumen/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createRoot(document.getElementById('root')).render(
