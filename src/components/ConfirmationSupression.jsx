d4807c5e805b55db17f0cfb5c64cd21d43f8ed24
Janelle <2264502@cegeplimoilou.ca>
2024-09-20 16:15:44 -0400

    Création component "confirmationSupression" et ajout photo #20

--- src/components/ConfirmationSupression.jsx
+++ src/components/ConfirmationSupression.jsx
@@ -1,30 +1,29 @@
 export default function ConfirmationSupression({estOuvertConfirmation, toggleModalConfirmation}) {
-    return (
-        <>
+    return (<>
             <div>
-                {
-                    estOuvertConfirmation && (
-                        <div className="popup">
-                            <div className={"card border-0 "}>
-                                <h1>Êtes-vous certain de vouloir supprimer cet oiseau ?</h1>
-                                <p> Cette action est définitive.</p>
-                                <div className={"row"}>
+                {estOuvertConfirmation && (<div className="popup">
+                        <div className={"card border-0 "}>
+                            <row>
+                                <h2 className={"my-3 card-title"}>Êtes-vous certain de vouloir supprimer cet oiseau
+                                    ?</h2>
+                                <h3 className={"text-danger my-3"}> Cette action est définitive.</h3>
+                                <hr/>
 
-
-                                </div>
-                                <div className={"row"}>
-                                    <div className={"btn-wrapper text-center d-flex justify-content-evenly modal-footer"}>
-                                        <button className={"btn btn-dark "} data-toggle="modal"
-                                                onClick={toggleModalConfirmation}>Annuler
-                                        </button>
-                                        {/*Ajouter le onClick qui appellera la fonction de suppression de l'oiseau, aussi ajouter une confirmation de supression dans le bas de l'écran par exemple.*/}
-                                        <button className={"btn btn-danger"} data-toggle="modal">Supprimer</button>
-                                    </div>
+                                <img src={"src/assets/images/confirmation.png"} className={"img-confirmation my-3"}
+                                     alt="are you sure ? Please reconsider your life choices as you may want to keep this bird."/>
+                                <hr/>
+                            </row>
+                            <div className={"row"}>
+                                <div className={"btn-wrapper text-center d-flex justify-content-evenly modal-footer"}>
+                                    <button className={"btn btn-dark "} data-toggle="modal"
+                                            onClick={toggleModalConfirmation}>Annuler
+                                    </button>
+                                    {/*Ajouter le onClick qui appellera la fonction de suppression de l'oiseau, aussi ajouter une confirmation de supression dans le bas de l'écran par exemple.*/}
+                                    <button className={"btn btn-danger"} data-toggle="modal">Supprimer</button>
                                 </div>
                             </div>
                         </div>
-                    )}
+                    </div>)}
             </div>
-        </>
-    )
+        </>)
 }
