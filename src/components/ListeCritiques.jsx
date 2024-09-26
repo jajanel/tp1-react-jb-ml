a179154a356a6a1004f1f519ac3b641528feaad7
maek <2033482@cegeplimoilou.ca>
2024-09-26 11:07:44 -0400

    créer le composant Critique.jsx #32

--- src/components/ListeCritiques.jsx
+++ src/components/ListeCritiques.jsx
@@ -1,4 +1,5 @@
 import MaCritique from "./MaCritique.jsx";
+import {dataCritiques} from "../assets/critiques.js";
 
 export default function ListeCritiques(props) {
 
@@ -22,32 +23,9 @@ export default function ListeCritiques(props) {
                                 <h5 className="text-uppercase card-title">Visualiser les critiques</h5>
                                 <hr/>
                                 {/*Ici il faut utiliser une .map sur les différentes critiques existantes.*/}
-                                <div className="bg-light rounded-3 p-2 m-2">
 
-                                    <div className="pb-4">
-                                        <label className="form-label">ID</label>
-                                        <input type="" className="form-control" id="note" disabled/>
-                                    </div>
-                                    <div className="pb-4">
-                                        <label className="form-label">Note globale:</label>
-                                        <input type="number" className="form-control" id="note" disabled/>
-                                    </div>
-                                    <div className="pb-4">
 
-                                        <label className="form-label">Tempérament:</label>
-                                        <input
-                                            type="text" className="form-control" id="temperament" disabled/>
-                                    </div>
-                                    <div className="pb-4">
-                                        <label className="form-label">Beauté:</label>
-                                        <input type="text" className="form-control" id="beaute" disabled/>
-                                    </div>
-                                    <div className="pb-4">
 
-                                        <label className="form-label">Utilisation:</label> <input
-                                        type="text" className="form-control" id="utilisation" disabled/>
-                                    </div>
-                                </div>
                                 <hr/>
                             </div>
 
