4699f12cd50c4f1a378809d2ee65601230002120
JanelleB2 <2264502@cegeplimoilou.ca>
2024-09-28 20:13:19 -0400

    Mise en local storage des listes donnees #37
    Co-Authored-By: Maek Lorman <157525653+Alakazouu@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar.jsx";
-import {dataOiseau} from "./assets/oiseaux.js";
 import CatalogueOiseaux from "./components/CatalogueOiseaux.jsx";
-import {dataCritiques} from "./assets/critiques.js";
+import {dataOiseau as donnesOiseauxDefaut} from "./assets/oiseaux.js";
+import {dataCritiques as donneesCritiquesDefaut} from "./assets/critiques.js";
+
+// Fonction pour obtenir les données du local storage ou utiliser les données par défaut
+const getDonneesLocalStorage = (key, donneesParDefaut) => {
+    const donnees = localStorage.getItem(key);
+    return donnees ? JSON.parse(donnees) : donneesParDefaut;
+};
 
 function App() {
-
     const [categorieSelectionne, setCategorieSelectionne] = useState("tous");
-    const dataOiseauState = useState(dataOiseau);
-    const dataCritiqueState = useState(dataCritiques);
+
+    // Initialiser le state avec les données du local storage, ou sinon avec les données par défaut (la liste directement prise du fichier "json")
+    const [dataOiseau, setDataOiseau] = useState(() => getDonneesLocalStorage("dataOiseau", donnesOiseauxDefaut));
+    const [dataCritiques, setDataCritiques] = useState(() => getDonneesLocalStorage("dataCritiques", donneesCritiquesDefaut));
 
 
-    //Change la valeur de la catégorie montrée dans le catalogue en utilisant le setter setCategorieSelectionne
+    // Resauvegarder les données dans le local storage à chauque changement
+    useEffect(() => {
+        localStorage.setItem("dataOiseau", JSON.stringify(dataOiseau));
+    }, [dataOiseau]);
+
+    useEffect(() => {
+        localStorage.setItem("dataCritiques", JSON.stringify(dataCritiques));
+    }, [dataCritiques]);
+
+
+    // Changer la valeur de la catégorie montrée dans le catalogue en utilisant le setter setCategorieSelectionne
     const handleChangementCategorie = (categorieOiseau) => {
         setCategorieSelectionne(categorieOiseau);
     };
 
-    //Filtre les oiseaux selon la catégorie sélectionnée par l'utilsateur (valeur par défaut est tous les oiseaux)
-    const oiseauxFiltre = categorieSelectionne === "tous"
-        ? dataOiseau
-        : dataOiseau.filter(oiseau => oiseau.categorie === categorieSelectionne);
+    // Filtrer les oiseaux selon la catégorie sélectionnée par l'utilisateur (valeur par défaut est tous les oiseaux)
+    const oiseauxFiltre = categorieSelectionne === "tous" ? dataOiseau : dataOiseau.filter(oiseau => oiseau.categorie === categorieSelectionne);
 
-
-    return (
-        <>
-            <Navbar surChangementCategorie={handleChangementCategorie} dataCritiqueState={dataCritiqueState} dataOiseauState={dataOiseauState} oiseauxFiltre={oiseauxFiltre}/>
-            <CatalogueOiseaux oiseauxFiltre={oiseauxFiltre} dataOiseauState={dataOiseauState} dataCritiqueState={dataCritiqueState}/>
-        </>
-    );
+    return (<>
+            <Navbar surChangementCategorie={handleChangementCategorie}
+                    dataCritiqueState={[dataCritiques, setDataCritiques]} dataOiseauState={[dataOiseau, setDataOiseau]}
+                    oiseauxFiltre={oiseauxFiltre}/>
+            <CatalogueOiseaux oiseauxFiltre={oiseauxFiltre} dataOiseauState={[dataOiseau, setDataOiseau]}
+                              dataCritiqueState={[dataCritiques, setDataCritiques]}/>
+        </>);
 }
 
 export default App;
