bcea9939166304074b44005afbd27d02bbcfdde0
Janelle <2264502@cegeplimoilou.ca>
2024-10-04 16:18:39 -0400

    Changement apparence Catalogue Statistique #31

--- src/components/CatalogueStatistiques.jsx
+++ src/components/CatalogueStatistiques.jsx
@@ -5,12 +5,6 @@ export default function CatalogueStatistiques(props) {
 
     function moyenne(categorie){
 
-        let avg = 0;
-
-        console.log(getNotes());
-
-
-        return avg;
     }
 
     return (
@@ -18,28 +12,28 @@ export default function CatalogueStatistiques(props) {
             <div>
                 {
                     props.estOuvertStatistiques &&
-                    (<div className="popup">
+                    (<div className="border-bottom border-3 border-black pb-3">
                             <div className="card border-0 ">
-                                <div className="row">
-                                    <h1 className="my-3 card-title">Statistiques</h1>
-                                    <hr/>
-                                    <div>
-                                        <ul>
-                                            <li>Tous: {moyenne("tous")}/100</li>
-                                            <li>Poules: {moyenne("poule")}/100</li>
-                                            <li>Canards: {moyenne("canard")}/100</li>
-                                            <li>Oies: {moyenne("oie")}/100</li>
-                                            <li>Dindes: {moyenne("dinde")}/100</li>
-                                        </ul>
+                                <div className="row text-center">
+                                    <div className="card-header pb-3">
+                                    <h1 className="my-3 card-title text-center">Statistiques</h1>
+                                    <h6 className="card-subtitle"> Les oiseaux du catalogue ci-dessous sont maintenant triés par note.</h6>
                                     </div>
                                     <hr/>
-                                    <Statistique/>
+                                    <div className="row d-flex text-center pb-3">
+                                        <div className="col">Poules:<br/> {moyenne("poule")}/100</div>
+                                        <div className="col">Canards:<br/> {moyenne("canard")}/100</div>
+                                        <div className="col">Oies: <br/>{moyenne("oie")}/100</div>
+                                            <div className="col">Dindes: <br/>{moyenne("dinde")}/100</div>
+                                    </div>
+
                                     <hr/>
                                 </div>
                                 <div className="row">
                                     <div className="btn-wrapper text-center d-flex justify-content-evenly modal-footer">
                                         <button className="btn btn-dark "
-                                                onClick={() => props.toggleModalStatistiques("tous")}>Fermer
+                                                onClick={() => props.toggleModalStatistiques("tous")}>
+                                            Fermer le mode statistique
                                         </button>
                                     </div>
                                 </div>
