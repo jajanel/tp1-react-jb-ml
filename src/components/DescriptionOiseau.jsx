66fae19778b3e8bd05b325da21a3da91d31a7aab
Janelle <2264502@cegeplimoilou.ca>
2024-10-09 16:34:16 -0400

    Cleanup code {" "} #23

--- src/components/DescriptionOiseau.jsx
+++ src/components/DescriptionOiseau.jsx
@@ -7,16 +7,16 @@ export default function DescriptionOiseau(props) {
             <div>
                 {props.estOuvertDescription &&
                     (<div className="popup">
-                            <div className={"card border-0 "}>
-                                <div className={"card-header bg-white"}>
-                                    <h5 className={"card-title text-uppercase"}>Description</h5>
+                            <div className="card border-0 ">
+                                <div className="card-header bg-white">
+                                    <h5 className="card-title text-uppercase">Description</h5>
                                 </div>
-                                <div className={"card-body"}>
-                                    <div className={"row"}>
+                                <div className="card-body">
+                                    <div className="row">
                                         <div className="col-xl-4 col-xxl-3 col-md-12 col-lg-6 align-content-center">
-                                            <div className={"ajouterPhoto rounded-3"}>
-                                                <img src={props.srcImage} className={"card-img "}
-                                                     alt={"image selectionnée par l'utilisateur"}/>
+                                            <div className="ajouterPhoto rounded-3">
+                                                <img src={props.srcImage} className="card-img "
+                                                     alt="image selectionnée par l'utilisateur"/>
                                             </div>
                                             <div className={"pb-4 text-start"}>
                                                 <label className="form-label">ID</label>
@@ -39,10 +39,9 @@ export default function DescriptionOiseau(props) {
                                                     <option value="">Dinde</option>
                                                 </select>
                                             </div>
-                                            <div className={"pb-4"}>
+                                            <div className="pb-4">
                                                 <label htmlFor="race" className="form-label">Race</label>
-                                                <select
-                                                    className="form-select" disabled>
+                                                <select className="form-select" disabled>
                                                     <option defaultValue={props.race}> {props.race}</option>
                                                     {/*Ici il faut utiliser une .map sur les différentes catégories existante.
                                                      La catégorie en cours ne sera pas en double dans la liste de choix à sélectionner*/}
@@ -52,12 +51,12 @@ export default function DescriptionOiseau(props) {
                                                     <option value="">Dinde</option>
                                                 </select>
                                             </div>
-                                            <div className={"pb-4"}>
+                                            <div className="pb-4">
 
                                                 <label htmlFor="prix" className="form-label">Prix</label>
-                                                <p className={"form-control disabled"}>{props.prix}</p>
+                                                <p className="form-control disabled">{props.prix}</p>
                                             </div>
-                                            <div className={"pb-4"}>
+                                            <div className="pb-4">
 
                                                 <label className="form-label">Origine</label>
                                                 {/*Ici il faut utiliser une .map sur les différentes catégories existante.
@@ -72,27 +71,23 @@ export default function DescriptionOiseau(props) {
                                                     <option value="oceanie">Océanie</option>
                                                 </select>
                                             </div>
-                                            <div className={"pb-4"}>
+                                            <div className="pb-4">
 
                                                 <label className="form-label">Date de publication</label>
                                                 <br/>
-                                                <input disabled type="date" className="form-control disabled"
-                                                       id="datePublication"
-                                                       name="datePublication" value={props.date}/>
+                                                <input disabled type="date" className="form-control disabled" id="datePublication" name="datePublication" value={props.date}/>
                                             </div>
                                         </div>
 
 
                                     </div>
                                 </div>
-                                <div className={"row"}>
+                                <div className="row">
                                     <div
-                                        className={"btn-wrapper text-center d-flex justify-content-evenly modal-footer"}>
-                                        <button className={"btn btn-danger "}
-                                                onClick={props.toggleModalDescription}>Annuler
+                                        className="btn-wrapper text-center d-flex justify-content-evenly modal-footer">
+                                        <button className="btn btn-danger" onClick={props.toggleModalDescription}>Annuler
                                         </button>
-                                        <button className={"btn btn-success"}
-                                                onClick={props.toggleModalModifierDescription}>Modifier
+                                        <button className="btn btn-success" onClick={props.toggleModalModifierDescription}>Modifier
                                         </button>
                                     </div>
                                 </div>
