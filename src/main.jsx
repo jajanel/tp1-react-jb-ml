d0a041a106a3bfb02a5553b982fc0c0b106344e6
JanelleB2 <2264502@cegeplimoilou.ca>
2024-10-11 21:27:05 -0400

    Import d'icônes bootstrap #55

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 //Importation de boostrap et un thème de bootswatch pour le style
 import 'bootswatch/dist/lumen/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
