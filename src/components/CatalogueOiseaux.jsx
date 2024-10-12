b612f5120bf9f3fc1cc1b9b68588442e9f2e59ed
JanelleB2 <2264502@cegeplimoilou.ca>
2024-10-11 21:29:23 -0400

    Changement bouton et marge catalogue #54

--- src/components/CatalogueOiseaux.jsx
+++ src/components/CatalogueOiseaux.jsx
@@ -3,6 +3,7 @@ import CarteProduit from "./CarteProduit.jsx";
 import {useState} from "react";
 import CatalogueStatistiques from "./CatalogueStatistiques.jsx";
 import {getNoteOiseauSpecifique} from "../classes/gestionCatalogueCritique.js";
+import Footer from "./Footer.jsx";
 
 export default function CatalogueOiseaux(props) {
     const [estOuvert, setEstOuvert] = useState(false);
@@ -18,23 +19,25 @@ export default function CatalogueOiseaux(props) {
 
     return (<>
         {/*afficher la section des stats*/}
-        {estOuvertStatistiques&&<CatalogueStatistiques
+        {estOuvertStatistiques && <CatalogueStatistiques
             dataCritiqueState={props.dataCritiqueState}
             estOuvertStatistiques={estOuvertStatistiques}
             fermerStatistiquesToggle={() => props.fermerStatistiquesToggle()}
         />}
-        <div className={"row"}>
-            <button className="btn btn-sm btn-secondary" onClick={toggleModalAjouterOiseau}>Ajouter un oiseau</button>
-            <AjouterOiseau
-                estOuvert={estOuvert}
-                toggleModal={toggleModalAjouterOiseau}
-                dataCritiqueState={[dataOiseau, setDataOiseau]}
-                dataOiseauState={[dataCritiques, setDataCritiques]}
+        <div className="container-fluid ">
 
-            />
-        </div>
-        <div className="container-fluid px-5">
-            <div className="row">
+            <div className="row mb-4">
+                <button className="btn btn-secondary btn-lg" onClick={toggleModalAjouterOiseau}>Ajouter un oiseau
+                </button>
+                <AjouterOiseau
+                    estOuvert={estOuvert}
+                    toggleModal={toggleModalAjouterOiseau}
+                    dataCritiqueState={[dataOiseau, setDataOiseau]}
+                    dataOiseauState={[dataCritiques, setDataCritiques]}
+
+                />
+            </div>
+            <div className="row px-5">
                 {/*Boucle pour afficher les cartes produits filtrées soit par leur notes moyenne(si stats est ouvert) ou normalement (oiseauFiltre)*/}
                 {(props.oiseauxTriBool[0] ? props.oiseauxFiltre.toSorted((a, b) => getNoteOiseauSpecifique(b.idOiseau) - getNoteOiseauSpecifique(a.idOiseau)) : props.oiseauxFiltre).map((oiseau) => (
                     <div className="col-xl-4 col-xxl-3 col-md-6 col-lg-6 align-content-center" key={oiseau.idOiseau}>
@@ -52,5 +55,6 @@ export default function CatalogueOiseaux(props) {
                 ))}
             </div>
         </div>
+
     </>);
 }
